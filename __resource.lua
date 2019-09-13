b43b82cf4529a96e7d8008401e487a06a95642db
Neddings <github@neddings.com>
2019-09-13 10:21:46 +0200

    English localisation

--- __resource.lua
+++ __resource.lua
@@ -1,31 +1,30 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 description 'ESX Hifi'
 
 version '1.0.0'
 
 server_scripts {
-        '@es_extended/locale.lua',
-        'locales/fr.lua',
-	'server/main.lua',
-	'config.lua'
+    '@es_extended/locale.lua',
+    'locales/fr.lua',
+    'server/main.lua',
+    'config.lua'
 }
 
 client_script {
-        '@es_extended/locale.lua',
-	'client/main.lua',
-        'locales/fr.lua',
-	'config.lua'
+    '@es_extended/locale.lua',
+    'client/main.lua',
+    'locales/fr.lua',
+    'config.lua'
 }
 
 ui_page('html/index.html')
 
 files {
-	'html/index.html',
-	'html/app.js'
+    'html/index.html',
+    'html/app.js'
 }
 
 dependencies {
-        'es_extended'
+    'es_extended'
 }
-
---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+Locales ['en'] = {
+	['put_hifi'] = 'You have just dropped the BoomBox',
+	['get_hifi'] = 'Pick up the BoomBox',
+	['play_music'] = 'Start the music',
+	['stop_music'] = 'Stop the music',
+	['volume_music'] = 'Adjust the volume',
+	['hifi_alreadyOne'] = 'You already have a BoomBox on you',
+	['set_volume'] = 'Enter the volume level (between 0 and 100)',
+	['play_id'] = 'Enter the id of the YouTube video',
+	['sound_limit'] = 'The volume must be between 0 and 100',
+	['hifi_help'] = 'press ~ INPUT_CONTEXT ~ to adjust the music'
+}
